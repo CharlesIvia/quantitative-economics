717a35bd74dafe14a7da7beb713f8073c7513398
Charles Ivia <iviacharles@gmail.com>
2021-03-08 20:37:30 +0300

    Exploring inheritance in classes

--- intermidiate/classes_review.py
+++ intermidiate/classes_review.py
@@ -88,7 +88,7 @@ class Circle:
     # Method
 
     def get_circumference(self):
-        #Circle.pi referencing class object attribute or self.pi
+        # Circle.pi referencing class object attribute or self.pi
         return self.radius * Circle.pi * 2
 
 
@@ -98,3 +98,71 @@ print(my_circle.pi)
 print(my_circle.radius)
 print(my_circle.get_circumference())
 print(my_circle.area)
+
+# Inheritance in classes
+
+# Base class
+
+
+class Animal(object):
+    def __init__(self, age):
+        self.age = age
+        self.name = None
+
+    def get_age(self):
+        return self.age
+
+    def get_name(self):
+        return self.name
+
+    def set_age(self, newage):
+        self.age = newage
+
+    def set_name(self, newname=""):
+        self.name = newname
+
+    def __str__(self):
+        return "animal: " + str(self.name) + ": " + str(self.age)
+
+
+# Derived class
+
+
+class Person(Animal):
+    def __init__(self, name, age):
+        Animal.__init__(self, age)  # Call Animal constructor
+        self.set_name(name)  # Call Animal's method
+        self.friends = []  # add new data attribute
+
+    def get_friends(self):
+        return self.friends
+
+    # new methods
+    def add_friend(self, fname):
+        if fname not in self.friends:
+            self.friends.append(fname)
+
+    def speak(self):
+        print("hello")
+
+    def age_diff(self, other):
+        diff = self.age - other.age
+        print(abs(diff), "year difference.")
+
+    # override Animal's __str__ method
+    def __str__(self):
+        return "person: " + str(self.name) + ": " + str(self.age)
+
+
+gats = Person("Gats", 22)
+print(gats)
+
+ethan = Person("Ethan", 8)
+
+gats.add_friend("Gichobi")
+gats.add_friend("Kithuu")
+gats.add_friend("Kamau")
+
+print(gats.get_friends())
+print(gats.speak)
+print(gats.age_diff(ethan))
